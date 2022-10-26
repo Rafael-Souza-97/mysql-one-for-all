c3967d0fb4b08fe0ef5cef5ced0368a834300e34
Rafael Souza <souza_rafael.97@outlook.com>
2022-10-26 14:50:02 -0300

    Adiciona Requisito 11

---
+++ desafio11.sql
@@ -0,0 +1,13 @@
+SELECT 
+    songs.song_name AS 'nome_musica',
+    CASE songs.song_name
+        WHEN 'The Bard’s Song' THEN REPLACE(songs.song_name, 'The Bard’s Song', 'The QA’s Song')
+        WHEN 'O Medo de Amar é o Medo de Ser Livre' THEN REPLACE(songs.song_name, 'O Medo de Amar é o Medo de Ser Livre', 'O Medo de Code Review é o Medo de Ser Livre')
+        WHEN 'Como Nossos Pais' THEN REPLACE(songs.song_name, 'Como Nossos Pais', 'Como Nossos Pull Requests')
+        WHEN 'BREAK MY SOUL' THEN REPLACE(songs.song_name, 'BREAK MY SOUL', 'BREAK MY CODE')
+        WHEN 'ALIEN SUPERSTAR' THEN REPLACE(songs.song_name, 'ALIEN SUPERSTAR', 'ALIEN SUPERDEV')
+    END AS 'novo_nome'
+FROM
+    SpotifyClone.songs
+GROUP BY songs.song_name HAVING novo_nome <> nome_musica
+ORDER BY nome_musica DESC;
